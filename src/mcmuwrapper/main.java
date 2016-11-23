3eefee6a71095412b750528f616ad7d0ba27ead4
minizbot2012 <bradleyfwin@gmail.com>
2016-11-23 14:11:15 -0500

    Use github API to gain latest release download URL

--- src/mcmuwrapper/main.java
+++ src/mcmuwrapper/main.java
@@ -4,13 +4,17 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.net.URLConnection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class main {
     public static void main(String[] args) {
         try {
-            String ver = getVer();
-            main.saveUrl("mcmu.jar", String.format("https://github.com/TeamEndAllReality/MCMU/releases/download/%s/MCMU.jar", ver));
+            String inf = main.getURL("https://api.github.com/repos/TeamEndAllReality/MCMU/releases/latest");
+            Pattern pat = Pattern.compile("\"browser_download_url\": \"(.*)\"");
+            Matcher m = pat.matcher(inf);
+            String dlurl = m.group(0);
+            main.saveUrl("mcmu.jar", dlurl);
             File file = new File("mcmu.jar");
             URL url = file.toURI().toURL();
             URL[] urls = new URL[]{url};
@@ -22,7 +26,19 @@ public class main {
             // empty catch block
         }
     }
-
+    public static String getURL(String getURL) throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(getURL);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString();
+    }
     public static String getVer() throws Exception {
         StringBuilder result = new StringBuilder();
         URL url = new URL("https://raw.githubusercontent.com/TeamEndAllReality/MCMU/master/ver");

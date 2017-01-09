06c85028583b1b341c90f981656b18b699c82c19
bradl <bradleyfwiN@gmail.com>
2017-01-09 18:41:15 -0500

    No longer download MCMU itself (loads from github)

--- src/mcmuwrapper/main.java
+++ src/mcmuwrapper/main.java
@@ -12,10 +12,7 @@ public class main {
             Matcher m = pat.matcher(inf);
             m.find();
             String dlurl = m.group(1);
-            main.saveUrl("mcmu.jar", dlurl);
-            File file = new File("mcmu.jar");
-            URL url = file.toURI().toURL();
-            URL[] urls = new URL[]{url};
+            URL[] urls = new URL[]{new URL(dlurl)};
             URLClassLoader cl = new URLClassLoader(urls);
             Class cls = cl.loadClass("mcmu.MCMU");
             cls.newInstance();
@@ -32,44 +29,9 @@ public class main {
         BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
         String line;
         while ((line = rd.readLine()) != null) {
-            result.append(line+"\n");
+            result.append(line + "\n");
         }
         rd.close();
         return result.toString();
-    }
-    public static String getVer() throws Exception {
-        StringBuilder result = new StringBuilder();
-        URL url = new URL("https://raw.githubusercontent.com/TeamEndAllReality/MCMU/master/ver");
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String line;
-        while ((line = rd.readLine()) != null) {
-            result.append(line);
-        }
-        rd.close();
-        return result.toString();
-    }
-
-    public static void saveUrl(String filename, String urlString) throws IOException {
-        BufferedInputStream in = null;
-        FileOutputStream fout = null;
-        try {
-            int count;
-            in = new BufferedInputStream(new URL(urlString).openStream());
-            fout = new FileOutputStream(filename);
-            byte[] data = new byte[1024];
-            while ((count = in.read(data, 0, 1024)) != -1) {
-                fout.write(data, 0, count);
-            }
-        }
-        finally {
-            if (in != null) {
-                in.close();
-            }
-            if (fout != null) {
-                fout.close();
-            }
-        }
     }
 }

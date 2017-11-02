acbd5de1589ba9263de72a3f60bb2c8ffab5f6d8
mini <bradleyfwin@gmail.com>
2017-11-02 08:55:02 -0400

    Beginning of generic URL Wrapper, we are no long MCMUWrapper

--- src/mcmuwrapper/Wrapper.java
+++ src/mcmuwrapper/Wrapper.java
@@ -1,15 +1,17 @@
 package mcmuwrapper;
 
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.*;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 import java.util.regex.*;
 
-public class main {
+public class Wrapper {
     public static void main(String[] args) {
         try {
-            String inf = main.getURL("https://api.github.com/repos/TeamEndAllReality/MCMU/releases/latest");
+            String inf = Wrapper.getURL("https://api.github.com/repos/TeamEndAllReality/MCMU/releases/latest");
             Pattern pat = Pattern.compile("\"browser_download_url\":\"(.*?)\"", Pattern.CASE_INSENSITIVE);
             Matcher m = pat.matcher(inf);
             m.find();
@@ -17,13 +19,29 @@ public class main {
             URL jarURL = new URL(dlurl);
             JarURLConnection conn = (JarURLConnection) (new URL(jarURL, "jar:"+dlurl+"!/").openConnection());
             Manifest mf = conn.getManifest();
-            ClassLoader cl = URLClassLoader.newInstance(new URL[] {jarURL}, main.class.getClassLoader());
+            ClassLoader cl = URLClassLoader.newInstance(new URL[] {jarURL}, Wrapper.class.getClassLoader());
             Attributes attr = mf.getMainAttributes();
             Class cls = Class.forName(attr.getValue(Attributes.Name.MAIN_CLASS), true, cl);
             cls.newInstance();
         }
         catch (Exception file) {
             // empty catch block
+        }
+    }
+    public Wrapper(String[] args) {
+
+    }
+    public void bootstrapJar(URL jfs[]) throws NoSuchMethodException {
+        URLClassLoader classLoader = URLClassLoader.newInstance(null);
+        Method m = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+        for (URL jf : jfs) {
+            try {
+                m.invoke(classLoader, jf);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
         }
     }
     public static String getURL(String getURL) throws Exception {
